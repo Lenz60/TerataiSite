45883cf3ba3d09c37fef5bc70755068510d4a7b7
Rafly Andrian <raflyandrian@gmail.com>
2024-04-28 23:37:54 +0700

    Basic catalog UI is done

---
+++ resources/js/Pages/Catalog.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="border-2 border-green-400 m-2 flex flex-row">
+        <div class="border-2 border-blue-400 m-2 flex flex-col w-full">
+            <div
+                class="border-2 border-yellow-300 m-2 justify-center text-center"
+            >
+                <h1>This is a Title</h1>
+            </div>
+            <div class="border-2 border-red-400 m-2">
+                <div class="border-2 border-cyan-400 m-2 flex flex-row">
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            Likewise the innovational path that we have chosen
+                            requires the analysis of forms of influence.
+                        </p>
+                    </div>
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            Likewise the innovational path that we have chosen
+                            requires the analysis of forms of influence.
+                        </p>
+                    </div>
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            Likewise the innovational path that we have chosen
+                            requires the analysis of forms of influence.
+                        </p>
+                    </div>
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            Likewise the innovational path that we have chosen
+                            requires the analysis of forms of influence.
+                        </p>
+                    </div>
+                </div>
+                <div class="border-2 border-cyan-400 m-2 flex flex-row">
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            On the other hand the innovational path that we have
+                            chosen significantly conditions the creation of new
+                            propositions.
+                        </p>
+                    </div>
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            On the other hand the innovational path that we have
+                            chosen significantly conditions the creation of new
+                            propositions.
+                        </p>
+                    </div>
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            On the other hand the innovational path that we have
+                            chosen significantly conditions the creation of new
+                            propositions.
+                        </p>
+                    </div>
+                    <div class="border-2 border-green-500 m-2">
+                        <p>
+                            On the other hand the innovational path that we have
+                            chosen significantly conditions the creation of new
+                            propositions.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div data-theme="forest">
+        <h1 class="text-lg">This is home page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
