a48fc67e7ddea5ca8a789918909d8de97a80db5a
Rafly Andrian <raflyandrian@gmail.com>
2024-04-30 23:40:37 +0700

    - Added Navbar - Progress on basic layout

---
+++ resources/js/Components/Home/Navbar.vue
@@ -0,0 +1,101 @@
+<template>
+    <div data-themes="foliage">
+        <header class="navbar bg-base-100 shadow-md w-screen">
+            <div class="flex-none"></div>
+            <div class="flex-1">
+                <a href="/">
+                    <img
+                        src="/images/logo.png"
+                        alt=""
+                        class="btn rounded-md btn-ghost normal-case invisible xl:visible md:visible sm:invisible"
+                    />
+                </a>
+            </div>
+            <div v-if="!noAuth" class="px-5">
+                <div v-if="$page.props.auth.user" class="menu flex-row-reverse">
+                    <div class="flex">
+                        <ul class="menu menu-horizontal px-1">
+                            <li>
+                                <details class="text-primary">
+                                    <summary>
+                                        <p
+                                            class="text-md font-semibold font-montserrat px-2 break-words"
+                                        >
+                                            {{ $page.props.auth.user.name }}
+                                        </p>
+                                    </summary>
+                                    <ul
+                                        class="flex flex-col bg-base-200 bg-opacity-70 w-full rounded-md text-center justify-center"
+                                    >
+                                        <li>
+                                            <Link
+                                                :href="route('dashboard')"
+                                                method="get"
+                                            >
+                                                <p class="text-sm text-center">
+                                                    Dashboard
+                                                </p>
+                                            </Link>
+                                        </li>
+                                        <!-- <li>
+                                            <Link
+                                                :href="route('dashboard.admin')"
+                                                method="get"
+                                            >
+                                                <p class="text-sm text-center">
+                                                    Dashboard Admin
+                                                </p>
+                                            </Link>
+                                        </li> -->
+                                        <li>
+                                            <Link
+                                                :href="route('profile.edit')"
+                                                method="get"
+                                            >
+                                                <p class="text-sm text-center">
+                                                    Profile
+                                                </p>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                :href="route('logout')"
+                                                method="post"
+                                            >
+                                                <p class="text-sm text-center">
+                                                    Log out
+                                                </p>
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </details>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div v-else>
+                    <Link :href="route('login')">
+                        <p class="btn btn-ghost">Sign in</p>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+import { Link } from "@inertiajs/vue3";
+export default {
+    components: {
+        Link,
+    },
+    props: {
+        noAuth: Object,
+    },
+    setup(props) {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- resources/js/Pages/Catalog.vue
+++ resources/js/Pages/Catalog.vue
@@ -1,10 +1,46 @@
 <template>
-    <div class="border-2 border-green-400 m-2 flex flex-row">
+    <Navbar></Navbar>
+    <div
+        class="border-2 border-green-400 m-2 items-center flex flex-col bg-base-100"
+    >
+        <!-- Hero -->
+        <div class="bg-accent flex w-full flex-col border-2 border-black">
+            <div class="w-[60%] m-5 text-wrap border-2 border-cyan-300">
+                <h1 class="indent-8 text-5xl p-2 font-semibold">Catalog</h1>
+                <p class="p-2 indent-5">
+                    The diverse and plentiful experience the understanding of
+                    nature of resource saving technologies directly depends on
+                    economic practicability of decisions made. Thus the
+                    established organisation structure requires the analysis of
+                    strengthening the democratic system. It should be noted that
+                    the established organisation structure allows to complete
+                    important implementation tasks of strengthening the
+                    democratic system.
+                </p>
+            </div>
+        </div>
         <div class="border-2 border-blue-400 m-2 flex flex-col w-full">
-            <div
-                class="border-2 border-yellow-300 m-2 justify-center text-center"
-            >
-                <h1>This is a Title</h1>
+            <div class="flex flex-row items-center justify-center">
+                <div
+                    class="border-2 m-2 card rounded-md p-2 text-wrap border-black w-fit h-fit"
+                >
+                    <h1 class="text-3xl w-fit">Indoor Furniture</h1>
+                </div>
+                <div
+                    class="border-2 m-2 card rounded-md p-2 text-wrap border-black w-fit h-fit"
+                >
+                    <h1 class="text-3xl w-fit">Outdoor Furniture</h1>
+                </div>
+                <div
+                    class="border-2 m-2 card rounded-md p-2 text-wrap border-black w-fit h-fit"
+                >
+                    <h1 class="text-3xl w-fit">Handicraft Furniture</h1>
+                </div>
+                <div
+                    class="border-2 m-2 card rounded-md p-2 text-wrap border-black w-fit h-fit"
+                >
+                    <h1 class="text-3xl w-fit">Root Furniture</h1>
+                </div>
             </div>
             <div class="border-2 border-red-400 m-2">
                 <div
@@ -37,31 +73,37 @@
                     </div>
                 </div>
                 <!-- <div class="border-2 border-cyan-400 m-2 flex flex-row">
-                    <div
-                        v-for="furniture in furnitures"
-                        class="border-2 border-green-500 m-2 w-[500px] flex flex-col items-center justify-center"
-                    >
-                        <div>
-                            <img
-                                :src="image + furniture.image"
-                                alt=""
-                                class="w-[100px] h-[100px]"
-                            />
-                            <h2 class="p-2">${{ furniture.price }}</h2>
-                            <h1 class="p-2">{{ furniture.description }}</h1>
+                        <div
+                            v-for="furniture in furnitures"
+                            class="border-2 border-green-500 m-2 w-[500px] flex flex-col items-center justify-center"
+                        >
+                            <div>
+                                <img
+                                    :src="image + furniture.image"
+                                    alt=""
+                                    class="w-[100px] h-[100px]"
+                                />
+                                <h2 class="p-2">${{ furniture.price }}</h2>
+                                <h1 class="p-2">{{ furniture.description }}</h1>
+                            </div>
                         </div>
-                    </div>
-                </div> -->
+                    </div> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import Navbar from "@/Components/Home/Navbar.vue";
 import { router } from "@inertiajs/vue3";
 import Swal from "sweetalert2";
 export default {
     props: ["furnitures"],
+    components: {
+        AuthenticatedLayout,
+        Navbar,
+    },
     setup() {
         const image = "http://inventory.test/storage/";
         return { image };
