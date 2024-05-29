ab8baa0a5bbb9aa68b8216520eace9efa2dde78b
Rafly Andrian <raflyandrian@gmail.com>
2024-05-30 02:45:54 +0700

    Table cart is joined with users and furniture table to be able to display the joined data like furniture description in the cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
     //
     public function index(){
-        return Inertia::render('Cart');
+        $cart = DB::table('cart')
+        ->join('furniture', 'cart.furniture_id', '=', 'furniture.uuid')
+        ->join('users', 'cart.user_id', '=', 'users.uuid')
+        ->select('cart.id', 'cart.user_id', 'cart.furniture_id','furniture.image','furniture.description', 'cart.qty','cart.total_price' )
+        ->get();
+        // dd($cart);
+        // return Inertia::render('Cart');
+        return Inertia::render('Cart', ['carts' => $cart]);
     }
 }
