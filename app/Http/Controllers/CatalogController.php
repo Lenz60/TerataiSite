2d8a5b011ec271f064a6ced23833b4d693db3295
Rafly Andrian <raflyandrian@gmail.com>
2024-05-30 02:42:18 +0700

    Any items that added to cart via catalog view, is now inputted to cart table

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -29,7 +30,24 @@ class CatalogController extends Controller
     }
 
     public function cart(Request $request){
-        // dd($request->all());
+        // dd($request->uuid);
+        $payloadFurniture = $request->uuid;
+        $furnitureSelected = DB::table('furniture')
+        ->where('uuid', $payloadFurniture)
+        ->first();
+
+        // dd($furnitureSelected);
+
+        // $cart = DB::table('cart');
+        $user = Auth::user();
+        // dd($user->uuid);
+        Cart::create([
+            'id' => fake()->uuid(),
+            'user_id' => $user->uuid,
+            'furniture_id' => $payloadFurniture,
+            'qty' => 1,
+            'total_price' => $furnitureSelected->price
+        ]);
     }
 
     public function indexFiltered(){
