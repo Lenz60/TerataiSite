9a100b6541501dec5b396277ea4a524a797ded3d
Rafly Andrian <raflyandrian@gmail.com>
2024-06-06 00:35:12 +0700

    feat: Add checkout functionality to CartController

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrdersPayment;
+use App\Models\OrdersProduction;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -28,6 +31,40 @@ class CartController extends Controller
         ->where('furniture_id', $request->uuid)
         ->delete();
         return redirect()->route('cart.index');
+
+    }
+
+    public function checkout(Request $request){
+        // dd($request->all());
+        $carts = $request->cart;
+        foreach($carts as $cart){
+            // dd($cart);
+            $id = fake()->uuid();
+            Order::create([
+                'id' => $id,
+                'cart_id' => $cart['id'],
+                'track_code' => 'TRK'.rand(1000,9999),
+                'qty' => $cart['qty'],
+                'total_price' => $cart['total_price'],
+            ]);
+
+            OrdersPayment::create([
+                'order_id' => $id,
+                'payment_method' => 'pending',
+                'payment_status' => 'pending',
+            ]);
+
+            OrdersProduction::create([
+                'order_id' => $id,
+                'production_status' => 'In Production',
+            ]);
+
+        }
+
+        //redirect to cart.index with payload of a cartCheckout => request->all()
+        // return redirect()->route('cart.index', ['cartCheckout' => $request->all()]);
+        // return Inertia::render('Cart', ['cartCheckout' => $request->all()]);
+        // return response()->json(['status' => 'success'], 200);
 
     }
 }
