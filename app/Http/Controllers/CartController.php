389c54688a65da6c799adc30f689ee6f93e14845
Rafly Andrian <raflyandrian@gmail.com>
2024-06-21 01:03:58 +0700

    feat: Separate order items into OrderItems model
    Refactor the `Order` model by removing the `qty` and `total_price` fields, and create a new `OrderItems` model. This change improves code structure and organization by separating order items into their own model. It also allows for better management and tracking of individual order items.
    
    Also fixes bug where the sended price is not match with the qty from cart, and added total_price to Order that defines total all order price

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\OrderItems;
 use App\Models\OrdersPayment;
 use App\Models\OrdersProduction;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; // Add this import statement
 use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
@@ -16,11 +17,14 @@ class CartController extends Controller
         $cart = DB::table('cart')
         ->join('furniture', 'cart.furniture_id', '=', 'furniture.uuid')
         ->join('users', 'cart.user_id', '=', 'users.uuid')
-        ->select('cart.id', 'cart.user_id', 'cart.furniture_id','cart.preorder','furniture.image','furniture.description', 'cart.qty','cart.total_price' )
+        ->select('cart.id', 'cart.user_id', 'cart.furniture_id','cart.preorder','furniture.image','furniture.description','furniture.price', 'cart.qty','cart.total_price' )
         ->orderBy('cart.created_at', 'desc')
         ->get();
         // dd($cart);
         // return Inertia::render('Cart');
+        //Add cart to the sessionstorage named cart
+        // $carts = $cart;
+        // $request->session()->put('cart', $carts);
         return Inertia::render('Cart', ['carts' => $cart]);
     }
 
@@ -49,24 +53,44 @@ class CartController extends Controller
 
     public function checkout(Request $request){
         // dd($request->all());
+        // dd($request->all());
         $carts = $request->cart;
+        $orderId = fake()->uuid;
+        $totalOrderPrice = $request->totalPrice;
+        Order::create([
+            'id' => $orderId,
+            'total_price' => $totalOrderPrice,
+            'track_code' => 'TRK'.rand(1000,9999),
+        ]);
+        // dd($request->totalPrice);
         foreach($carts as $cart){
-            // dd($cart);
-            Order::create([
-                'id' => $cart['id'],
-                'track_code' => 'TRK'.rand(1000,9999),
+
+            // dd($cart['total_price']);
+            if($cart['preorder']==0){
+                $preorder = 'false';
+            }else{
+                $preorder = 'true';
+            }
+
+            OrderItems::create([
+                //TODO: add Original price in OrderItems table
+                'order_id' => $orderId,
+                'user_id' => $cart['user_id'],
+                'furniture_id'=>$cart['furniture_id'],
+                'preorder'=>$preorder,
                 'qty' => $cart['qty'],
                 'total_price' => $cart['total_price'],
+                'track_code' => 'TRK'.rand(1000,9999),
             ]);
 
             OrdersPayment::create([
-                'order_id' => $cart['id'],
+                'order_id' => $orderId,
                 'payment_method' => 'pending',
                 'payment_status' => 'pending',
             ]);
 
             OrdersProduction::create([
-                'order_id' => $cart['id'],
+                'order_id' => $orderId,
                 'production_status' => 'In Production',
             ]);
 
