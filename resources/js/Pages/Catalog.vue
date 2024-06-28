0cb13db11d394928e3584f5b15d223bd180a1937
Rafly Andrian <raflyandrian@gmail.com>
2024-06-29 04:48:49 +0700

    feat: Update Catalog.vue to display furniture length instead of depth
    Refactor the color radio to get the correct color (Still on progress)

--- resources/js/Pages/Catalog.vue
+++ resources/js/Pages/Catalog.vue
@@ -1,8 +1,6 @@
 <template>
     <Navbar :CartCount="cartCounts"></Navbar>
-    <div
-        class="m-2 items-center border-2 border-red-400 flex flex-col bg-base-100"
-    >
+    <div class="m-2 items-center flex flex-col bg-base-100">
         <!-- Hero -->
         <div class="bg-success text-neutral-content flex w-full flex-col">
             <div class="w-[60%] m-5 text-wrap">
@@ -21,9 +19,7 @@
         </div>
         <div class="m-2 flex flex-col w-full">
             <div class="flex flex-row items-center justify-center">
-                <div
-                    class="m-2 border-2 border-cyan-300 card rounded-md p-2 text-wrap w-fit h-fit"
-                >
+                <div class="m-2 card rounded-md p-2 text-wrap w-fit h-fit">
                     <Link :href="route('catalog.index')">
                         <button class="btn btn-sm btn-accent h-fit w-fit">
                             <h1 class="text-xl font-normal w-fit h-fit">All</h1>
@@ -84,15 +80,15 @@
                 </div>
             </div>
             <div
-                class="bg-base-200 border-2 border-green-400 h-full text-accent-content bg-center m-2 mx-10 card shadow-xl rounded-md"
+                class="bg-base-200 h-full text-accent-content bg-center m-2 mx-10 card shadow-xl rounded-md"
             >
                 <div
-                    class="border-2 border-green-500 w-auto h-full grid grid-cols-6 gap-1 grid-rows-2 m-2"
+                    class="w-auto h-full grid grid-cols-6 gap-1 grid-rows-2 m-2"
                 >
                     <div
-                        v-for="furniture in paginatedFurnitures"
+                        v-for="furniture in groupedFurniture"
                         :key="furniture.uuid"
-                        class="border-2 border-yellow-400 card rounded-md items-center h-fit shadow-sm hover:shadow-xl hover:border-2 hover:border-neutral w-fit m-2 p-5 flex flex-col"
+                        class="card rounded-md items-center h-full bg-base-100 shadow-sm hover:shadow-xl hover:border-2 hover:border-neutral w-fit m-2 p-5 flex flex-col"
                     >
                         <div>
                             <img
@@ -118,37 +114,72 @@
                                     <h1 class="font-bold">Materials :</h1>
                                     <p>{{ furniture.wood_type }}</p>
                                 </div>
-                                <h1 class="p-1 text-wrap">
-                                    Color: {{ furniture.color }}
-                                </h1>
-                                <h1 class="p-1 text-wrap">
+                                <div
+                                    class="text-center items-center flex flex-row"
+                                >
+                                    <h1 class="p-1 text-wrap">Color:</h1>
+
+                                    <div v-for="color in furniture.color">
+                                        <div class="flex flex-row">
+                                            <label class="label cursor-pointer">
+                                                <input
+                                                    type="radio"
+                                                    :name="
+                                                        'radio' + furniture.code
+                                                    "
+                                                    :value="color"
+                                                    class="radio radio-sm"
+                                                    :style="{
+                                                        color: color,
+                                                        background: color,
+                                                    }"
+                                                    @change="
+                                                        selectedColor(
+                                                            $event,
+                                                            furniture.code
+                                                        )
+                                                    "
+                                                    :checked="
+                                                        color ===
+                                                        furniture.color[0]
+                                                    "
+                                                />
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- <h1 class="p-1 text-wrap">
                                     Ready stock : {{ furniture.stock }}
-                                </h1>
+                                </h1> -->
                                 <h2 class="p-1 text-center font-bold">
                                     ${{ furniture.price }}
                                 </h2>
                             </div>
-                            <button
-                                @click="
-                                    addToCart(
-                                        furniture.uuid,
-                                        furniture.description,
-                                        furniture.stock
-                                    )
-                                "
-                                class="btn btn-success rounded-md p-2"
+                            <div
+                                class="items-center justify-center text-center"
                             >
-                                <div v-if="furniture.stock > 0">
-                                    Add to cart
-                                </div>
-                                <div v-else>Pre order</div>
-                            </button>
+                                <button
+                                    @click="
+                                        addToCart(
+                                            furniture.code,
+                                            furniture.description,
+                                            furniture.color
+                                        )
+                                    "
+                                    class="btn btn-success rounded-md p-2"
+                                >
+                                    <div v-if="furniture.stock > 0">
+                                        Add to cart
+                                    </div>
+                                    <div v-else>Pre order</div>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="join justify-center items-center m-2">
                     <button
-                        v-for="page in totalPages"
+                        v-for="page in totalPages - 1"
                         :key="page"
                         @click="changePage(page)"
                         :class="{ 'btn-enabled': page === currentPage.value }"
@@ -182,9 +213,10 @@ export default {
         // const furnitures = ref(props.furnitures);
         const currentPage = ref(1);
         const itemsPerPage = ref(10);
+        const colorSelected = ref([]);
         // console.log(state);
         const cartCount = props.cartCounts;
-        console.log(props.cartCounts);
+        console.log(props.furnitures.length);
 
         const paginatedFurnitures = computed(() => {
             const start = (currentPage.value - 1) * itemsPerPage.value;
@@ -202,6 +234,7 @@ export default {
         const changePage = (page) => {
             console.log("Changing page to:", page); // Debugging line
             currentPage.value = page;
+            console.log(currentPage.value);
         };
 
         // console.log("Total pages :" + totalPages.page);
@@ -214,14 +247,69 @@ export default {
             totalPages,
             changePage,
             cartCount,
+            colorSelected,
         };
     },
+    computed: {
+        groupedFurniture() {
+            // console.log(this.furnitures);
+            let grouped = {};
+            this.furnitures.forEach((furniture) => {
+                // let colorLowercase = furniture.color.toLowerCase(); // Convert the color to lowercase
+                if (!grouped[furniture.code]) {
+                    grouped[furniture.code] = furniture;
+                    grouped[furniture.code].color = [furniture.color]; // Wrap the color in an array
+                } else {
+                    let colors = new Set(grouped[furniture.code].color);
+                    // console.log(colors.toLowerCase);
+                    colors.add(furniture.color); // Add the color to the set
+                    grouped[furniture.code].color = Array.from(colors); // Convert the set back to an array
+                }
+            });
+            console.log(Object.values(grouped));
+
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            console.log(start, end);
+            return Object.values(grouped).slice(start, end);
+            // return Object.values(grouped);
+        },
+        // ...
+
+        mergedFurniture() {
+            let merged = [...this.paginatedFurnitures];
+            this.groupedFurniture.forEach((item) => {
+                if (!merged.find((furniture) => furniture.code === item.code)) {
+                    merged.push(item);
+                }
+            });
+            return merged;
+        },
+    },
     methods: {
-        async addToCart(uuid, desc, stock) {
-            // console.log(this.user);
+        selectedColor(event, code) {
+            console.log(code);
+            //Make colorSelected an array that contains code and color based on the parameter received
+            this.colorSelected[code] = event.target.value;
+            // this.colorSelected = event.target.value;
+        },
+        getColor(code, color) {
+            if (color.length > 1) {
+                if (color.includes(this.colorSelected[code])) {
+                    return this.colorSelected[code];
+                } else {
+                    return color[0];
+                }
+            } else {
+                return color[0];
+            }
+        },
+        addToCart(code, desc, color) {
+            color = this.getColor(code, color);
+            // console.log(color);
             if (this.user) {
                 Swal.fire({
-                    text: desc + " added to cart",
+                    text: color + " " + desc + " added to cart",
                     target: "#custom-target",
                     customClass: {
                         container: "position-absolute",
@@ -229,27 +317,24 @@ export default {
                     confirmButtonColor: "#049806",
                     toast: true,
                     position: "bottom-right",
-                    timer: 2000, // 3000 milliseconds (3 seconds)
+                    timer: 1000, // 3000 milliseconds (3 seconds)
                     timerProgressBar: true, // Display a progress bar
                     showConfirmButton: false,
                 });
-                console.log(stock);
-                if (stock == "0") {
-                    // console.log(true);
-                    router.post(route("catalog.cart"), {
-                        _method: "post",
-                        uuid: uuid,
-                        preorder: true,
-                    });
-                } else {
-                    router.post(route("catalog.cart"), {
-                        _method: "post",
-                        uuid: uuid,
-                        preorder: false,
-                    });
-                }
+                // console.log(stock);
+
+                // console.log(true);
+                router.post(route("catalog.cart"), {
+                    _method: "post",
+                    code: code,
+                    color: color,
+                });
+                // console.log(this.colorSelected);
+                this.colorSelected = null;
+
+                // console.log(this.colorSelected);
             } else {
-                const result = await Swal.fire({
+                const result = Swal.fire({
                     title: "You are not logged in",
                     text: "If you want to add this item to your cart, please login first",
                     icon: "info",
