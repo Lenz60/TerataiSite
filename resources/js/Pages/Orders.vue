baf9eeca216c1bd0331762386102b5810ca0c3ca
Rafly Andrian <raflyandrian@gmail.com>
2024-08-11 02:53:31 +0700

    Basic view for purchased order is done

---
+++ resources/js/Pages/Orders.vue
@@ -0,0 +1,39 @@
+<template>
+    <Head title="Purchased Order" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Purchased Order
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="bg-base-200 overflow-hidden shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        Purchased user order will show up here
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+export default {
+    components: {
+        AuthenticatedLayout,
+        Head,
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
